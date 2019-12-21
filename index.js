42117fa8074cace8c7c1e9fd48aeb4926cc9983e
PaBLyS <pavlobokhan@gmail.com>
2019-12-21 12:15:02 +0200

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('./app/models');
 const config = require('./config');
 
 const app = express();
+
+app.use(cors());
+
 config.express(app);
 config.routes(app);
 
